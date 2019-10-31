bc06c9bfd1d91dce89c7e58036adc13d044d3f8e
Ana victoria <anivi98@hotmail.com>
2019-10-31 10:55:46 -0500

    setup

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-  
-var express = require('express');
-var motorRender = require('express-handlebars');
+const express = require('express'),
+    engines = require('consolidate');
+
 var app = express();
 
-var fs = require('fs');
-console.log(__dirname);
+
+app.engine('handlebars', engines.handlebars);
+
+app.set('views', './views');
+app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
-app.engine('handlebars', motorRender());
-app.set('view engine', 'handlebars');
-app.use(express.urlencoded({extended: true}));
-app.engine('handlebars', motorRender({
-    defaultLayout: false,
-}));
-app.get('/',(function(req,res){
 
-res.sendfile('./index.html')
-}));
+//---------------//
 
-app.get('/tienda',(function(req,res){
-   
-    
-    
-      res.render('tienda');
-    }));
+//Index
+app.get( '/', ( req, res ) => {
+    res.render('index');
+});
+
+//Tienda
+app.get('/tienda', (req,res) => {
+    res.render('tienda');
+});
+
     
 app.listen(5000);
